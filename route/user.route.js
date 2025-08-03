4843177ead0068b0bcef43caad94024d2a421ae6
Alif Ahmed <alifahmed102@gmail.com>
2025-08-03 21:07:18 +0600

    Login controller

--- route/user.route.js
+++ route/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { registerUserController, verifyEmailController } from '../controllers/user.controller.js'
+import { loginController, registerUserController, verifyEmailController } from '../controllers/user.controller.js'
 // import auth from '../middleware/auth.js'
 // import upload from '../middleware/multer.js'
 
@@ -7,7 +7,7 @@ const userRouter = Router()
 
 userRouter.post('/register',registerUserController)
 userRouter.post('/verify-email',verifyEmailController)
-// userRouter.post('/login',loginController)
+userRouter.post('/login',loginController)
 // userRouter.get('/logout',auth,logoutController)
 // userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatar)
 // userRouter.put('/update-user',auth,updateUserDetails)
