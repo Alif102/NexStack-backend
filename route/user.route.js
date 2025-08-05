ef3c893e2b1dae53e733bb98e4a2c75f686895fd
Alif Ahmed <alifahmed102@gmail.com>
2025-08-05 15:13:22 +0600

    updateUser

--- route/user.route.js
+++ route/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { loginController, logoutController, registerUserController, uploadAvatar, verifyEmailController } from '../controllers/user.controller.js'
+import { forgotPasswordController, loginController, logoutController, registerUserController, updateUserDetails, uploadAvatar, verifyEmailController } from '../controllers/user.controller.js'
 import auth from './middleware/auth.js'
 import upload from './middleware/multer.js'
 // import auth from '../middleware/auth.js'
@@ -12,8 +12,8 @@ userRouter.post('/verify-email',verifyEmailController)
 userRouter.post('/login',loginController)
 userRouter.get('/logout',auth,logoutController)
 userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatar)
-// userRouter.put('/update-user',auth,updateUserDetails)
-// userRouter.put('/forgot-password',forgotPasswordController)
+userRouter.put('/update-user',auth,updateUserDetails)
+userRouter.put('/forgot-password',forgotPasswordController)
 // userRouter.put('/verify-forgot-password-otp',verifyForgotPasswordOtp)
 // userRouter.put('/reset-password',resetpassword)
 // userRouter.post('/refresh-token',refreshToken)
